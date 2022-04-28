9684334699e8c41a159337b01cfb5ce35f3b2b27
JIMm2527 <jimmymh87@gmail.com>
2022-04-27 19:08:50 -0500

    Create Login.cs

---
+++ ALE_MexJet/Objetos/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ALE_MexJet.Objetos
+{
+    [Serializable]
+    public class Login
+    {
+        public string email { set; get; }
+        public string password { set; get; }
+    }
+    [Serializable]
+    public class nombreUsuario
+    {
+        public string nombre { set; get; }
+    }
+
+    public class FiltroEmail
+    {
+        public string email { get; set; }
+    }
+}
